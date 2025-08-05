0e943b07ac343b1ba1993473fc9c1b7e5b96bff1
만땅선생 <zocvo@naver.com>
2025-08-06 01:20:46 +0900

    script.js

--- script.js
+++ script.js
@@ -42,4 +42,6 @@ function drawChart(done,left){
     data:{labels:['완료','남은'],datasets:[{data:[done,left],backgroundColor:['#4caf50','#ccc']}]},
     options:{responsive:true,plugins:{legend:{position:'bottom'}}}
   });
+  window.startReading = startReading;
+
 }
